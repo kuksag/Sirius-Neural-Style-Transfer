989f718d4d5cc55cb0ea5c6b93957dedf19be796
kuksag <georgiy.kuksa@yandex.ru>
2018-07-28 12:22:04 +0300

    First Version

--- SiriusProject1.py
+++ SiriusProject1.py
@@ -1,31 +1,85 @@
-from flask import Flask, render_template, request, send_file, redirect, url_for
+from flask import Flask, render_template, request, redirect, url_for, session
 import os
+from flask_caching import Cache
 
 ###     FLASK    ###
 
 
+
 app = Flask(__name__)
+cache = Cache(app, config={'CACHE_TYPE': 'null'})
+
+i = 0
+
+app.config['UPLOAD_FOLDER'] = 'C:/Users/George/PycharmProjects/SiriusProject1'
+app.config['TEMPLATES_AUTO_RELOAD'] = True
+app.secret_key = b'_5#y2L"F4q8z\n\xec]/'
+
+@app.context_processor
+def override_url_for():
+    """
+    Generate a new token on every request to prevent the browser from
+    caching static  files.
+    """
+    return dict(url_for=dated_url_for)
+
+def dated_url_for(endpoint, **values):
+    if endpoint == 'static':
+        filename = values.get('filename', None)
+        if filename:
+            file_path = os.path.join(app.root_path,
+                                     endpoint, filename)
+            values['q'] = int(os.stat(file_path).st_mtime)
+    return url_for(endpoint, **values)
 
 
 @app.route('/', methods=['POST', 'GET'])
 def home():
 
+    if 'username' not in session:
+        global i
+        session['username'] = i
+        i += 1
+
     if request.method == 'POST':
+
         input_image = request.files['input']
-        style_image = request.files['style']
+        os.makedirs(os.path.join(app.config['UPLOAD_FOLDER'] + '/static/images/' + str(session['username'])), exist_ok=True)
+        input_image.save(os.path.join(app.config['UPLOAD_FOLDER'] + '/static/images/' +
+                         str(session['username']) + '/input.jpg'))
 
-        input_image.save(os.path.join('C:/Users/George/PycharmProjects/SiriusProject1/static/images/input.jpg'))
-        style_image.save(os.path.join('C:/Users/George/PycharmProjects/SiriusProject1/static/images/style.jpg'))
+        return redirect('/l1#work')
 
-        return redirect( url_for('result', _anchor='result'))
+    return render_template('./extends.html', showres = 0, show1 = 0, show2 = 0, num = str(session['username']),
+                           form1 = 1, form2 = 0)
 
-    return render_template('index.html')
+@app.route('/l1', methods=['POST', 'GET'])
+def load1():
+
+    if request.method == 'POST':
+        session['choose'] = request.form['slt']
+        return redirect('/result')
+
+    return render_template('./extends.html', showres=0, show1=1, show2=0, num = str(session['username']),
+                           form1=0, form2=1)
 
 @app.route('/result')
-def result():
-    #run()
-    return render_template('index1.html')
+def end():
+    print(session['choose'])
+    print(session['username'])
+    run_name = app.config['UPLOAD_FOLDER'] + '/neural.py'
+    model_name = app.config['UPLOAD_FOLDER'] + '/models/' + str(session['choose']) + '.pth'
+    in_image_path = app.config['UPLOAD_FOLDER'] + '/static/images/' + str(session['username']) + '/input.jpg'
+    output_image_path = app.config['UPLOAD_FOLDER'] + '/static/images/' + str(session['username']) + '/result.jpg'
 
+    os.system('python {} eval --cuda 0 --content-image {} --model {} --output-image {}'.format(run_name, in_image_path, model_name, output_image_path))
+    return redirect('/show#result')
+
+@app.route('/show')
+def show():
+    return render_template('./extends.html', showres=1, show1=1, show2=1, form1=0, form2=0,
+                           num=str(session['username']), style_num=session['choose'],
+                           path=url_for('static', filename='images/' + str(session['username']) + '/result.jpg'))
 
 if __name__ == '__main__':
     app.run()
---
+++ neural.py
@@ -0,0 +1,230 @@
+import argparse
+import os
+import sys
+import time
+import re
+
+import numpy as np
+import torch
+from torch.optim import Adam
+from torch.utils.data import DataLoader
+from torchvision import datasets
+from torchvision import transforms
+import torch.onnx
+from PIL import Image
+
+def load_image(filename, size=None, scale=None):
+    img = Image.open(filename)
+    if size is not None:
+        img = img.resize((size, size), Image.ANTIALIAS)
+    elif scale is not None:
+        img = img.resize((int(img.size[0] / scale), int(img.size[1] / scale)), Image.ANTIALIAS)
+    return img
+
+
+def save_image(filename, data):
+    img = data.clone().clamp(0, 255).numpy()
+    img = img.transpose(1, 2, 0).astype("uint8")
+    img = Image.fromarray(img)
+    img.save(filename)
+
+
+def gram_matrix(y):
+    (b, ch, h, w) = y.size()
+    features = y.view(b, ch, w * h)
+    features_t = features.transpose(1, 2)
+    gram = features.bmm(features_t) / (ch * h * w)
+    return gram
+
+
+def normalize_batch(batch):
+    # normalize using imagenet mean and std
+    mean = batch.new_tensor([0.485, 0.456, 0.406]).view(-1, 1, 1)
+    std = batch.new_tensor([0.229, 0.224, 0.225]).view(-1, 1, 1)
+    batch = batch.div_(255.0)
+    return (batch - mean) / std
+
+
+class TransformerNet(torch.nn.Module):
+    def __init__(self):
+        super(TransformerNet, self).__init__()
+        # Initial convolution layers
+        self.conv1 = ConvLayer(3, 32, kernel_size=9, stride=1)
+        self.in1 = torch.nn.InstanceNorm2d(32, affine=True)
+        self.conv2 = ConvLayer(32, 64, kernel_size=3, stride=2)
+        self.in2 = torch.nn.InstanceNorm2d(64, affine=True)
+        self.conv3 = ConvLayer(64, 128, kernel_size=3, stride=2)
+        self.in3 = torch.nn.InstanceNorm2d(128, affine=True)
+        # Residual layers
+        self.res1 = ResidualBlock(128)
+        self.res2 = ResidualBlock(128)
+        self.res3 = ResidualBlock(128)
+        self.res4 = ResidualBlock(128)
+        self.res5 = ResidualBlock(128)
+        # Upsampling Layers
+        self.deconv1 = UpsampleConvLayer(128, 64, kernel_size=3, stride=1, upsample=2)
+        self.in4 = torch.nn.InstanceNorm2d(64, affine=True)
+        self.deconv2 = UpsampleConvLayer(64, 32, kernel_size=3, stride=1, upsample=2)
+        self.in5 = torch.nn.InstanceNorm2d(32, affine=True)
+        self.deconv3 = ConvLayer(32, 3, kernel_size=9, stride=1)
+        # Non-linearities
+        self.relu = torch.nn.ReLU()
+
+    def forward(self, X):
+        y = self.relu(self.in1(self.conv1(X)))
+        y = self.relu(self.in2(self.conv2(y)))
+        y = self.relu(self.in3(self.conv3(y)))
+        y = self.res1(y)
+        y = self.res2(y)
+        y = self.res3(y)
+        y = self.res4(y)
+        y = self.res5(y)
+        y = self.relu(self.in4(self.deconv1(y)))
+        y = self.relu(self.in5(self.deconv2(y)))
+        y = self.deconv3(y)
+        return y
+
+
+class ConvLayer(torch.nn.Module):
+    def __init__(self, in_channels, out_channels, kernel_size, stride):
+        super(ConvLayer, self).__init__()
+        reflection_padding = kernel_size // 2
+        self.reflection_pad = torch.nn.ReflectionPad2d(reflection_padding)
+        self.conv2d = torch.nn.Conv2d(in_channels, out_channels, kernel_size, stride)
+
+    def forward(self, x):
+        out = self.reflection_pad(x)
+        out = self.conv2d(out)
+        return out
+
+
+class ResidualBlock(torch.nn.Module):
+    """ResidualBlock
+    introduced in: https://arxiv.org/abs/1512.03385
+    recommended architecture: http://torch.ch/blog/2016/02/04/resnets.html
+    """
+
+    def __init__(self, channels):
+        super(ResidualBlock, self).__init__()
+        self.conv1 = ConvLayer(channels, channels, kernel_size=3, stride=1)
+        self.in1 = torch.nn.InstanceNorm2d(channels, affine=True)
+        self.conv2 = ConvLayer(channels, channels, kernel_size=3, stride=1)
+        self.in2 = torch.nn.InstanceNorm2d(channels, affine=True)
+        self.relu = torch.nn.ReLU()
+
+    def forward(self, x):
+        residual = x
+        out = self.relu(self.in1(self.conv1(x)))
+        out = self.in2(self.conv2(out))
+        out = out + residual
+        return out
+
+
+class UpsampleConvLayer(torch.nn.Module):
+    """UpsampleConvLayer
+    Upsamples the input and then does a convolution. This method gives better results
+    compared to ConvTranspose2d.
+    ref: http://distill.pub/2016/deconv-checkerboard/
+    """
+
+    def __init__(self, in_channels, out_channels, kernel_size, stride, upsample=None):
+        super(UpsampleConvLayer, self).__init__()
+        self.upsample = upsample
+        if upsample:
+            self.upsample_layer = torch.nn.Upsample(mode='nearest', scale_factor=upsample)
+        reflection_padding = kernel_size // 2
+        self.reflection_pad = torch.nn.ReflectionPad2d(reflection_padding)
+        self.conv2d = torch.nn.Conv2d(in_channels, out_channels, kernel_size, stride)
+
+    def forward(self, x):
+        x_in = x
+        if self.upsample:
+            x_in = self.upsample_layer(x_in)
+        out = self.reflection_pad(x_in)
+        out = self.conv2d(out)
+        return out
+
+def stylize_onnx_caffe2(content_image, args):
+    """
+    Read ONNX model and run it using Caffe2
+    """
+
+    assert not args.export_onnx
+
+    import onnx
+    import onnx_caffe2.backend
+
+    model = onnx.load(args.model)
+
+    prepared_backend = onnx_caffe2.backend.prepare(model, device='CUDA' if args.cuda else 'CPU')
+    inp = {model.graph.input[0].name: content_image.numpy()}
+    c2_out = prepared_backend.run(inp)[0]
+
+    return torch.from_numpy(c2_out)
+
+def stylize(args):
+    device = torch.device("cuda" if args.cuda else "cpu")
+
+    content_image = load_image(args.content_image, scale=args.content_scale)
+    content_transform = transforms.Compose([
+        transforms.ToTensor(),
+        transforms.Lambda(lambda x: x.mul(255))
+    ])
+    content_image = content_transform(content_image)
+    content_image = content_image.unsqueeze(0).to(device)
+
+    if args.model.endswith(".onnx"):
+        output = stylize_onnx_caffe2(content_image, args)
+    else:
+        with torch.no_grad():
+            style_model = TransformerNet()
+            state_dict = torch.load(args.model)
+            #print(state_dict.keys())
+            # remove saved deprecated running_* keys in InstanceNorm from the checkpoint
+            for k in list(state_dict.keys()):
+                if re.search(r'in\d+\.running_(mean|var)$', k):
+                    del state_dict[k]
+            style_model.load_state_dict(state_dict)
+            style_model.to(device)
+            if args.export_onnx:
+                assert args.export_onnx.endswith(".onnx"), "Export model file should end with .onnx"
+                output = torch.onnx._export(style_model, content_image, args.export_onnx)
+            else:
+                output = style_model(content_image).cpu()
+    save_image(args.output_image, output[0])
+
+def main():
+    main_arg_parser = argparse.ArgumentParser(description="parser for fast-neural-style")
+    subparsers = main_arg_parser.add_subparsers(title="subcommands", dest="subcommand")
+    
+    eval_arg_parser = subparsers.add_parser("eval", help="parser for evaluation/stylizing arguments")
+    eval_arg_parser.add_argument("--content-image", type=str, required=True,
+                                 help="path to content image you want to stylize")
+    eval_arg_parser.add_argument("--content-scale", type=float, default=None,
+                                 help="factor for scaling down the content image")
+    eval_arg_parser.add_argument("--output-image", type=str, required=True,
+                                 help="path for saving the output image")
+    eval_arg_parser.add_argument("--model", type=str, required=True,
+                                 help="saved model to be used for stylizing the image. If file ends in .pth - PyTorch path is used, if in .onnx - Caffe2 path")
+    eval_arg_parser.add_argument("--cuda", type=int, required=True,
+                                 help="set it to 1 for running on GPU, 0 for CPU")
+    eval_arg_parser.add_argument("--export_onnx", type=str,
+                                 help="export ONNX model to a given file")
+
+    args = main_arg_parser.parse_args()
+
+    if args.subcommand is None:
+        print("ERROR: specify either train or eval")
+        sys.exit(1)
+    if args.cuda and not torch.cuda.is_available():
+        print("ERROR: cuda is not available, try running on CPU")
+        sys.exit(1)
+
+    if args.subcommand == "train":
+        check_paths(args)
+        train(args)
+    else:
+        stylize(args)
+        
+if __name__ == "__main__":
+    main()
